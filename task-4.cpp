763c903866c0381173e8d62f9702c2d3a80c1d08
AbhayRautela1 <87599266+AbhayRautela1@users.noreply.github.com>
2021-11-25 07:56:54 +0530

    Update task-4.cpp

--- task-4.cpp
+++ task-4.cpp
@@ -10,20 +10,26 @@ class sample
         a=x;
     }
     
-    friend sample operator>(sample n,sample n3);
+    friend void operator<(sample n,sample n3);
 };
 
-sample operator>(sample n,sample n3)
+void operator<(sample n,sample n3)
 {
-    cout<<"Addition of two operator is = "<<n.a+n3.a;
+	if(n.a<n3.a)
+	{
+    		cout<<"Object 2 is greater than";
+    	}
+    	else{
+        	cout<<"Object 1 is greater than";
+        }
     exit(0);
 }
 
 int main()
 {
-    sample obj1,obj2,obj3;
+    sample obj1,obj2;
     obj1.setdata(10);
     obj2.setdata(30);
-    obj3=obj1>obj2;
+    obj1<obj2;
     return 0;
 }
